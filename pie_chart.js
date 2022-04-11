50700b1b63dd0540626e2b0517d4669c91acdf78
nickebinger <103428668+nickebinger@users.noreply.github.com>
2022-04-11 15:36:21 +0200

    update pie_chart.js

--- pie_chart.js
+++ pie_chart.js
@@ -8,44 +8,35 @@ data[0].E_Gruppe1_de;
 // The expression was set as Trim Path End 
 
 eval("var data=" +footage("data.json").sourceText);
-
 value = data[0].E_Gruppe1_Teil;
-
 easeOut(time,inPoint+0.2,inPoint+1.2,[0],[value]);
 
 // Animation second pie 
 // The expression was set as Trim Path End 
 
 eval("var data=" +footage("data.json").sourceText);
-
 valueStart = data[0].E_Gruppe1_Teil;
 valueEnd = data[0].E_Gruppe2_Teil; 
 valueEndCalc = valueStart+valueEnd;
-
 easeOut(time,inPoint+0.5,inPoint+1,[valueStart],[valueEndCalc]);
 
 // Animation third pie 
 // The expression was set as Trim Path End 
-
 eval("var data=" +footage("data.json").sourceText);
-
 value1 = data[0].E_Gruppe1_Teil;
 value2 = data[0].E_Gruppe2_Teil;
 value3 = data[0].E_Gruppe3_Teil; 
 valueStartCalc = value1+value2;
 valueEndCalc = valueStartCalc+value3;
-
 easeOut(time,inPoint+0.5,inPoint+1,[valueStartCalc],[valueEndCalc]);
 
 // Animation last pie 
 // The expression was set as Trim Path End 
 
 eval("var data=" +footage("data.json").sourceText);
-
 value1 = data[0].E_Gruppe1_Teil;
 value2 = data[0].E_Gruppe2_Teil;
 value3 = data[0].E_Gruppe3_Teil; 
 valueStartCalc = value1+value2;+value3;
 valueEndCalc = 100;
-
 easeOut(time,inPoint+0.5,inPoint+1.2,[valueStartCalc],[valueEndCalc]);
