31e8b0f36ccd046e8933be58549b59c2913c6306
nickebinger <103428668+nickebinger@users.noreply.github.com>
2022-04-11 15:35:34 +0200

    animation of the trade balance

---
+++ trade_balance.js
@@ -0,0 +1,5 @@
+// // Get data (String) from the imported .json File and display as a text
+// The expression was set as Source Text of a empty text layer
+
+eval("var data=" +footage("data.json").sourceText);
+data[0].Handelsbilanz;
