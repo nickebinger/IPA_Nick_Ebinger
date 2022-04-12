fe675e3a58b272e6bb3846cdebebfa15f7a453af
nickebinger <103428668+nickebinger@users.noreply.github.com>
2022-04-12 11:49:52 +0200

    updated trade balance

--- Expressions/trade_balance.js
+++ Expressions/trade_balance.js
@@ -1,4 +1,6 @@
-// // Get data (String) from the imported .json File and display as a text
+// ------------ TRADE BALANCE ------------
+
+// Get data (String) from the imported .json File and display as a text
 // The expression was set as Source Text of a empty text layer
 
 eval("var data=" +footage("data.json").sourceText);
